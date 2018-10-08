c4809270546f2e24f56c99aaba69d1eab5dc94f8
Timm Preetz <timm.preetz@aboutyou.de>
2018-10-08 15:39:12 +0200

    Add API tests

---
+++ src/types/__tests__/ProductSearchQuery.test.ts
@@ -0,0 +1,66 @@
+import {AttributeKey} from 'bapi/types/AttributeOrAttributeValueFilter';
+import {queryParamsFromProductSearchQuery} from 'bapi/types/ProductSearchQuery';
+
+it('Converts search query', () => {
+  expect(queryParamsFromProductSearchQuery(undefined)).toEqual(undefined);
+
+  expect(queryParamsFromProductSearchQuery({})).toMatchInlineSnapshot(
+    `Object {}`
+  );
+
+  // Leave some but not all values blank
+  expect(queryParamsFromProductSearchQuery({categoryId: 1234}))
+    .toMatchInlineSnapshot(`
+Object {
+  "filters[category]": 1234,
+}
+`);
+
+  expect(
+    queryParamsFromProductSearchQuery({
+      categoryId: 1235,
+      attributes: [
+        {
+          type: 'attributes',
+          key: 'emptyAttribute' as AttributeKey,
+          values: [],
+        },
+        {
+          type: 'attributes',
+          key: 'singleValue' as AttributeKey,
+          values: [1],
+        },
+        {
+          type: 'attributes',
+          key: 'listAttributes' as AttributeKey,
+          values: [1, 2, 3],
+        },
+        {
+          type: 'boolean',
+          key: 'on' as AttributeKey,
+          value: true,
+        },
+        {
+          type: 'boolean',
+          key: 'off' as AttributeKey,
+          value: false,
+        },
+      ],
+      maxPrice: 1999,
+      minPrice: 1,
+      term: 'search term',
+    })
+  ).toMatchInlineSnapshot(`
+Object {
+  "filters[category]": 1235,
+  "filters[emptyAttribute]": "",
+  "filters[listAttributes]": "1,2,3",
+  "filters[maxPrice]": 1999,
+  "filters[minPrice]": 1,
+  "filters[off]": "false",
+  "filters[on]": "true",
+  "filters[singleValue]": "1",
+  "filters[term]": "search term",
+}
+`);
+});
---
+++ src/types/__tests__/ProductWith.test.ts
@@ -0,0 +1,119 @@
+import {productWithQueryParameterValues} from 'bapi/types/ProductWith';
+
+it('Converts attribute filters', () => {
+  expect(
+    productWithQueryParameterValues({
+      attributes: 'all',
+    }).join(',')
+  ).toMatchInlineSnapshot(`"attributes"`);
+
+  expect(
+    productWithQueryParameterValues({
+      attributes: {
+        withKey: ['singleKey'],
+      },
+    }).join(',')
+  ).toMatchInlineSnapshot(`"attributes:key(singleKey)"`);
+
+  expect(
+    productWithQueryParameterValues({
+      attributes: {
+        withKey: ['keyA', 'keyB'],
+      },
+    }).join(',')
+  ).toMatchInlineSnapshot(`"attributes:key(keyA|keyB)"`);
+
+  expect(
+    productWithQueryParameterValues({
+      attributes: {
+        ofType: ['type1', 'type2'],
+      },
+    }).join(',')
+  ).toMatchInlineSnapshot(`"attributes:type(type1|type2)"`);
+});
+
+it('Converts image filters', () => {
+  // empty, because images are included by default
+  expect(
+    productWithQueryParameterValues({
+      images: 'all',
+    }).join(',')
+  ).toMatchInlineSnapshot(`""`);
+
+  expect(
+    productWithQueryParameterValues({
+      images: {
+        legacy: false,
+      },
+    }).join(',')
+  ).toMatchInlineSnapshot(`"images:legacy(false)"`);
+
+  expect(
+    productWithQueryParameterValues({
+      images: {
+        legacy: false,
+        withKey: ['singleKey'],
+      },
+    }).join(',')
+  ).toMatchInlineSnapshot(`"images:legacy(false):key(singleKey)"`);
+
+  expect(
+    productWithQueryParameterValues({
+      images: {
+        legacy: false,
+        withKey: ['keyA', 'keyB'],
+      },
+    }).join(',')
+  ).toMatchInlineSnapshot(`"images:legacy(false):key(keyA|keyB)"`);
+});
+
+it('Converts category filters', () => {
+  expect(
+    productWithQueryParameterValues({
+      categories: 'all',
+    }).join(',')
+  ).toMatchInlineSnapshot(`"categories"`);
+});
+
+it('Converts sibling filters', () => {
+  expect(
+    productWithQueryParameterValues({
+      siblings: 'all',
+    }).join(',')
+  ).toMatchInlineSnapshot(`"siblings"`);
+
+  expect(
+    productWithQueryParameterValues({
+      siblings: {
+        attributes: 'all',
+        categories: 'all',
+      },
+    }).join(',')
+  ).toMatchInlineSnapshot(`"siblings,siblings.attributes,siblings.categories"`);
+});
+
+it('Converts variant filters', () => {
+  expect(
+    productWithQueryParameterValues({
+      variants: 'all',
+    }).join(',')
+  ).toMatchInlineSnapshot(`"variants"`);
+
+  expect(
+    productWithQueryParameterValues({
+      variants: {
+        attributes: 'all',
+      },
+    }).join(',')
+  ).toMatchInlineSnapshot(`"variants,variants.attributes"`);
+
+  expect(
+    productWithQueryParameterValues({
+      variants: {
+        attributes: {
+          withKey: ['keyA'],
+        },
+      },
+    }).join(',')
+  ).toMatchInlineSnapshot(`"variants,variants.attributes:key(keyA)"`);
+});
