a609256efd11323b3633ab969b0a0f2d93e0ece4
Timm Preetz <timm.preetz@aboutyou.de>
2018-11-12 15:58:04 +0100

    Add tests for categoriesByIds

--- src/endpoints/categories/categoriesByIds.ts
+++ src/endpoints/categories/categoriesByIds.ts
@@ -8,6 +8,15 @@ export interface CategoriesByIdsEndpointParameters {
 
 export type CategoriesByIdsEndpointResponseData = BapiCategory[];
 
+/**
+ * Retrieve categories by their IDs
+ *
+ * Includes the legacy `depth` parameter (1 for the categories itself, 2 for level of children, etc.)
+ * and no further parameters, as the backend implementation is not yet fully aligned with the single
+ * category by ID endpoint options
+ *
+ * If no `depth` parameter is provided, the response will include *all* children in the tree
+ */
 export function createCategoriesByIdsEndpointRequest(
   parameters: CategoriesByIdsEndpointParameters,
 ): BapiCall<CategoriesByIdsEndpointResponseData> {
@@ -16,8 +25,12 @@ export function createCategoriesByIdsEndpointRequest(
     endpoint: `categories`,
     params: {
       ids: parameters.categoryIds.join(`,`),
-      with: parameters.depth && parameters.depth > 1 ? 'children' : undefined,
-      depth: parameters.depth,
+      ...(parameters.depth && parameters.depth > 1
+        ? {with: 'children'}
+        : undefined),
+      ...(parameters.depth !== undefined
+        ? {depth: parameters.depth}
+        : undefined),
       // TODO: `showHidden` not implemented correctly yet, see https://jira.collins.kg/browse/BAPI-413
     },
   };
