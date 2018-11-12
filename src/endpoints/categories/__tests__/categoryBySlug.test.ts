13c902a7f3931b2d70730be29227cf26c62118d3
Timm Preetz <timm.preetz@aboutyou.de>
2018-11-12 16:19:38 +0100

    Category by slug test

---
+++ src/endpoints/categories/__tests__/categoryBySlug.test.ts
@@ -0,0 +1,25 @@
+import {createCategoryBySlugEndpointRequest} from '../categoryBySlug';
+
+it('Builds correct query', () => {
+  expect(
+    createCategoryBySlugEndpointRequest({
+      slugPath: ['frauen', 'bekleidung'],
+      with: {
+        children: {
+          depth: 2,
+          includeHidden: true,
+        },
+      },
+    }),
+  ).toMatchInlineSnapshot(`
+Object {
+  "endpoint": "categories/frauen/bekleidung",
+  "method": "GET",
+  "params": Object {
+    "depth": 3,
+    "showHidden": "true",
+    "with": "descendants",
+  },
+}
+`);
+});
