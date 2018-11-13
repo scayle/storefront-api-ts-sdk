1b0dc387fd0e6772c15efaaf2674589ad15b1d8f
Timm Preetz <timm.preetz@aboutyou.de>
2018-11-13 15:44:26 +0100

    BapiClient categories tests

---
+++ src/helpers/__tests__/BapiClient.categories.test.ts
@@ -0,0 +1,87 @@
+import {BapiClient} from 'bapi/helpers/BapiClient';
+import {
+  nockWithBapiScope,
+  disableNetAndAllowBapiCors,
+} from 'bapi/test-helpers/nock';
+
+disableNetAndAllowBapiCors();
+
+it('Gets category by ID', async () => {
+  nockWithBapiScope()
+    .defaultReplyHeaders({'access-control-allow-origin': '*'})
+    .get('/v1/categories/20201')
+    .query({shopId: 139, depth: 1})
+    .replyWithFile(200, __dirname + '/responses/categories/byId.json', {
+      'Content-Type': 'application/json',
+    });
+
+  const bapi = new BapiClient({
+    host: 'https://api-cloud.example.com/v1/',
+    shopId: 139,
+    shopIdPlacement: 'query',
+  });
+
+  const response = await bapi.categories.getById(20201);
+
+  expect(response.id).toBe(20201);
+});
+
+it('Gets cateogires by IDs', async () => {
+  nockWithBapiScope()
+    .defaultReplyHeaders({'access-control-allow-origin': '*'})
+    .get('/v1/categories')
+    .query({shopId: 139, ids: '20201,20204'})
+    .replyWithFile(200, __dirname + '/responses/categories/byIds.json', {
+      'Content-Type': 'application/json',
+    });
+
+  const bapi = new BapiClient({
+    host: 'https://api-cloud.example.com/v1/',
+    shopId: 139,
+    shopIdPlacement: 'query',
+  });
+
+  const response = await bapi.categories.getByIds([20201, 20204]);
+
+  expect(response.length).toBe(2);
+});
+
+it('Gets category by path', async () => {
+  nockWithBapiScope()
+    .defaultReplyHeaders({'access-control-allow-origin': '*'})
+    .get('/v1/categories/frauen/bekleidung')
+    .query({shopId: 139, depth: 1})
+    .replyWithFile(200, __dirname + '/responses/categories/byPath.json', {
+      'Content-Type': 'application/json',
+    });
+
+  const bapi = new BapiClient({
+    host: 'https://api-cloud.example.com/v1/',
+    shopId: 139,
+    shopIdPlacement: 'query',
+  });
+
+  const response = await bapi.categories.getByPath(['frauen', 'bekleidung']);
+
+  expect(response.id).toBe(20204);
+});
+
+it('Gets root categories wishlist', async () => {
+  nockWithBapiScope()
+    .defaultReplyHeaders({'access-control-allow-origin': '*'})
+    .get('/v1/categories')
+    .query({shopId: 139, depth: 1})
+    .replyWithFile(200, __dirname + '/responses/categories/roots.json', {
+      'Content-Type': 'application/json',
+    });
+
+  const bapi = new BapiClient({
+    host: 'https://api-cloud.example.com/v1/',
+    shopId: 139,
+    shopIdPlacement: 'query',
+  });
+
+  const response = await bapi.categories.getRoots();
+
+  expect(response.length).toBe(3);
+});
