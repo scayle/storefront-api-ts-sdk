e735552868677873335e1f1a34f1dc4d1e08686d
Timm Preetz <timm.preetz@aboutyou.de>
2018-11-13 20:46:10 +0100

    Exclude non-stable files from test coverage

--- jest.config.js
+++ jest.config.js
@@ -15,4 +15,16 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {'^bapi/(.*)$': '<rootDir>/src/$1'},
   collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/index.ts',
+
+    /**
+     * Modeled product is not yet ready for shipping,
+     * hence excluded from coverage for now
+     */
+    '!src/helpers/ModeledBapiClient.ts',
+    '!src/helpers/productMapping.ts',
+    '!src/helpers/bapiProduct.ts',
+  ],
 };
