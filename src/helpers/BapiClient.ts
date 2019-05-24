6479ded95318cbe9cee70d30bea52fa3a04ef6f0
Timm Preetz <timm@preetz.us>
2019-05-24 09:46:30 +0200

    Destructure with default params

--- src/helpers/BapiClient.ts
+++ src/helpers/BapiClient.ts
@@ -322,10 +322,7 @@ export class BapiClient {
           const existingBasketItem = latestBasket.items.find(
             item => item.variant.id == itemToAdd.variantId,
           );
-          const quantity =
-            itemToAdd.quantity === undefined ? 1 : itemToAdd.quantity;
-          const params = itemToAdd.params || {};
-          const variantId = itemToAdd.variantId;
+          const {variantId, quantity = 1, params = {}} = itemToAdd;
 
           if (existingBasketItem) {
             if (quantity === 0) {
