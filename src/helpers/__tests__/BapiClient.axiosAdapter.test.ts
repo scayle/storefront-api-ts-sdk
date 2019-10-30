2fd00df11a8da64194cc2d39586f85d8f82d955a
Chintu Mohandas <chintu.mohandas@gmail.com>
2019-10-30 15:42:48 +0100

    unit tests

---
+++ src/helpers/__tests__/BapiClient.axiosAdapter.test.ts
@@ -0,0 +1,337 @@
+import {BapiClient} from 'bapi/helpers/BapiClient';
+import {
+  disableNetAndAllowBapiCors,
+} from 'bapi/test-helpers/nock';
+import MockAdapter from 'axios-mock-adapter';
+import Axios from 'axios';
+
+var mock = new MockAdapter(Axios);
+
+disableNetAndAllowBapiCors({shopIdHeader: true});
+
+describe("Axios adapter", () => {
+  it('should respond with desired response even with a custom axios adapter', async () => {
+
+    mock.onGet('https://api-cloud.example.com/v1/baskets/customer_2137901')
+      .reply(200, require(__dirname + '/responses/getBasket.json'));
+
+    const bapi = new BapiClient({
+      host: 'https://api-cloud.example.com/v1/',
+      shopId: 139,
+      axiosAdapter: mock.adapter()
+    });
+
+    const basketKey = 'customer_2137901';
+
+    const basketResponse = await bapi.basket.get(basketKey);
+
+    if (basketResponse.type !== 'success') {
+      fail('Expected success response');
+    }
+
+    expect(basketResponse.basket).toHaveProperty(`cost`);
+  });
+})
+
+
+// it('Get basket with error', async () => {
+//   nockWithBapiScope({shopIdHeader: true})
+//     .defaultReplyHeaders({'access-control-allow-origin': '*'})
+//     .get('/v1/baskets/customer_2137901')
+//     .reply(
+//       500,
+//       {},
+//       {
+//         'Content-Type': 'application/json',
+//       },
+//     );
+
+//   const bapi = new BapiClient({
+//     host: 'https://api-cloud.example.com/v1/',
+//     shopId: 139,
+//   });
+
+//   const basketKey = 'customer_2137901';
+
+//   const basketResponse = await bapi.basket.get(basketKey);
+
+//   if (basketResponse.type !== 'failure') {
+//     fail('Expected failure response');
+//   }
+// });
+
+// it('Basket: Add same variant twice', async () => {
+//   const bapi = new BapiClient({
+//     host: 'https://api-cloud.example.com/v1/',
+//     shopId: 139,
+//   });
+
+//   const basketKey = 'customer_2137901';
+
+//   nockWithBapiScope({shopIdHeader: true})
+//     .defaultReplyHeaders({'access-control-allow-origin': '*'})
+//     .post('/v1/baskets/customer_2137901/items', {
+//       variantId: 35149152,
+//       quantity: 1,
+//     })
+//     .replyWithFile(200, __dirname + '/responses/firstAddToBasket.json', {
+//       'Content-Type': 'application/json',
+//     });
+
+//   const firstAddToBasketResponse = await bapi.basket.addItem(
+//     basketKey,
+//     35149152,
+//   );
+
+//   nockWithBapiScope({shopIdHeader: true})
+//     .defaultReplyHeaders({'access-control-allow-origin': '*'})
+//     .post('/v1/baskets/customer_2137901/items', {
+//       variantId: 35149152,
+//       quantity: 1,
+//     })
+//     .replyWithFile(409, __dirname + '/responses/secondAddToBasket.json', {
+//       'Content-Type': 'application/json',
+//     });
+
+//   const secondTimeResponse = await bapi.basket.addItem(basketKey, 35149152);
+
+//   if (secondTimeResponse.type !== 'failure') {
+//     fail('Expected failure response');
+//     return;
+//   }
+
+//   expect(secondTimeResponse.kind).toEqual(0);
+//   expect(secondTimeResponse.basket).toEqual(firstAddToBasketResponse.basket);
+// });
+
+// it('Basket: Add variant failure 1', async () => {
+//   const bapi = new BapiClient({
+//     host: 'https://api-cloud.example.com/v1/',
+//     shopId: 139,
+//   });
+
+//   const basketKey = 'customer_2137901';
+
+//   nockWithBapiScope({shopIdHeader: true})
+//     .defaultReplyHeaders({'access-control-allow-origin': '*'})
+//     .post('/v1/baskets/customer_2137901/items', {
+//       variantId: 35149152,
+//       quantity: 1,
+//     })
+//     .reply(
+//       206,
+//       {},
+//       {
+//         'Content-Type': 'application/json',
+//       },
+//     );
+
+//   const response = await bapi.basket.addItem(basketKey, 35149152);
+
+//   expect(response.type).toBe('failure');
+// });
+
+// it('Basket: Add variant failure 1', async () => {
+//   const bapi = new BapiClient({
+//     host: 'https://api-cloud.example.com/v1/',
+//     shopId: 139,
+//   });
+
+//   const basketKey = 'customer_2137901';
+
+//   nockWithBapiScope({shopIdHeader: true})
+//     .defaultReplyHeaders({'access-control-allow-origin': '*'})
+//     .post('/v1/baskets/customer_2137901/items', {
+//       variantId: 35149152,
+//       quantity: 1,
+//     })
+//     .reply(
+//       424,
+//       {},
+//       {
+//         'Content-Type': 'application/json',
+//       },
+//     );
+
+//   const response = await bapi.basket.addItem(basketKey, 35149152);
+
+//   expect(response.type).toBe('failure');
+// });
+
+// it('Basket: Add variant failure 1', async () => {
+//   const bapi = new BapiClient({
+//     host: 'https://api-cloud.example.com/v1/',
+//     shopId: 139,
+//   });
+
+//   const basketKey = 'customer_2137901';
+
+//   nockWithBapiScope({shopIdHeader: true})
+//     .defaultReplyHeaders({'access-control-allow-origin': '*'})
+//     .post('/v1/baskets/customer_2137901/items', {
+//       variantId: 35149152,
+//       quantity: 1,
+//     })
+//     .reply(
+//       409,
+//       {},
+//       {
+//         'Content-Type': 'application/json',
+//       },
+//     );
+
+//   const response = await bapi.basket.addItem(basketKey, 35149152);
+
+//   expect(response.type).toBe('failure');
+// });
+
+// it('Basket: Add variant failure 1', async () => {
+//   const bapi = new BapiClient({
+//     host: 'https://api-cloud.example.com/v1/',
+//     shopId: 139,
+//   });
+
+//   const basketKey = 'customer_2137901';
+
+//   nockWithBapiScope({shopIdHeader: true})
+//     .defaultReplyHeaders({'access-control-allow-origin': '*'})
+//     .post('/v1/baskets/customer_2137901/items', {
+//       variantId: 35149152,
+//       quantity: 1,
+//     })
+//     .reply(
+//       412,
+//       {},
+//       {
+//         'Content-Type': 'application/json',
+//       },
+//     );
+
+//   const response = await bapi.basket.addItem(basketKey, 35149152);
+
+//   expect(response.type).toBe('failure');
+// });
+
+// it('Basket: Add variant failure 1', async () => {
+//   const bapi = new BapiClient({
+//     host: 'https://api-cloud.example.com/v1/',
+//     shopId: 139,
+//   });
+
+//   const basketKey = 'customer_2137901';
+
+//   nockWithBapiScope({shopIdHeader: true})
+//     .defaultReplyHeaders({'access-control-allow-origin': '*'})
+//     .post('/v1/baskets/customer_2137901/items', {
+//       variantId: 35149152,
+//       quantity: 1,
+//     })
+//     .reply(
+//       413,
+//       {},
+//       {
+//         'Content-Type': 'application/json',
+//       },
+//     );
+
+//   const response = await bapi.basket.addItem(basketKey, 35149152);
+
+//   expect(response.type).toBe('failure');
+// });
+
+// it('Basket: Add variant failure 1', async () => {
+//   const bapi = new BapiClient({
+//     host: 'https://api-cloud.example.com/v1/',
+//     shopId: 139,
+//   });
+
+//   const basketKey = 'customer_2137901';
+
+//   nockWithBapiScope({shopIdHeader: true})
+//     .defaultReplyHeaders({'access-control-allow-origin': '*'})
+//     .post('/v1/baskets/customer_2137901/items', {
+//       variantId: 35149152,
+//       quantity: 1,
+//     })
+//     .reply(
+//       400,
+//       {},
+//       {
+//         'Content-Type': 'application/json',
+//       },
+//     );
+
+//   const response = await bapi.basket.addItem(basketKey, 35149152);
+
+//   expect(response.type).toBe('failure');
+// });
+
+// it('Basket: Add same variant twice', async () => {
+//   const bapi = new BapiClient({
+//     host: 'https://api-cloud.example.com/v1/',
+//     shopId: 139,
+//   });
+
+//   nockWithBapiScope({shopIdHeader: true})
+//     .defaultReplyHeaders({'access-control-allow-origin': '*'})
+//     .delete('/v1/baskets/basket_1/items/item_1')
+//     .replyWithFile(200, __dirname + '/responses/firstAddToBasket.json', {
+//       'Content-Type': 'application/json',
+//     });
+
+//   const deleteItemResponse = await bapi.basket.deleteItem('basket_1', 'item_1');
+
+//   expect(deleteItemResponse.items.length).toBe(1);
+// });
+
+// it('Basket: Update item quantity', async () => {
+//   const bapi = new BapiClient({
+//     host: 'https://api-cloud.example.com/v1/',
+//     shopId: 139,
+//   });
+
+//   nockWithBapiScope({shopIdHeader: true})
+//     .defaultReplyHeaders({'access-control-allow-origin': '*'})
+//     .patch('/v1/baskets/basket_1/items/item_1', {
+//       quantity: 5,
+//     })
+//     .replyWithFile(200, __dirname + '/responses/firstAddToBasket.json', {
+//       'Content-Type': 'application/json',
+//     });
+
+//   const deleteItemResponse = await bapi.basket.updateItem(
+//     'basket_1',
+//     'item_1',
+//     5,
+//   );
+
+//   expect(deleteItemResponse.type).toBe('success');
+// });
+
+// it('Basket: Update item failure', async () => {
+//   const bapi = new BapiClient({
+//     host: 'https://api-cloud.example.com/v1/',
+//     shopId: 139,
+//   });
+
+//   nockWithBapiScope({shopIdHeader: true})
+//     .defaultReplyHeaders({'access-control-allow-origin': '*'})
+//     .patch('/v1/baskets/basket_1/items/item_1', {
+//       quantity: 5,
+//     })
+//     .reply(
+//       500,
+//       {},
+//       {
+//         'Content-Type': 'application/json',
+//       },
+//     );
+
+//   const deleteItemResponse = await bapi.basket.updateItem(
+//     'basket_1',
+//     'item_1',
+//     5,
+//   );
+
+//   expect(deleteItemResponse.type).toBe('failure');
+// });
