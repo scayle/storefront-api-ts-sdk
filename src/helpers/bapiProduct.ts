257c353c4e21afe292a13ccddcdf62aaca57730a
HenriBeck <henri.beck@aboutyou.com>
2020-02-24 20:08:14 +0100

    Check for length of attribute values

--- src/helpers/bapiProduct.ts
+++ src/helpers/bapiProduct.ts
@@ -24,7 +24,7 @@ export const getFirstAttributeValue = (
   }
 
   if (attribute.multiSelect) {
-    return attribute.values[0];
+    return attribute.values.length > 0 ? attribute.values[0] : undefined;
   }
 
   return attribute.values;
