e076373fcea36fa188e01edd6e1bb5b1e0ac1748
Timm Preetz <timm.preetz@aboutyou.de>
2020-04-20 15:40:31 +0200

    Add snapshots for v5 root categories behavior

---
+++ src/endpoints/categories/__tests__/categories.test.ts
@@ -0,0 +1,34 @@
+import {createCategoriesEndpointRequest} from '../categories';
+
+it('Builds correct query for root categories', () => {
+  expect(createCategoriesEndpointRequest({})).toMatchInlineSnapshot(`
+Object {
+  "endpoint": "categories",
+  "method": "GET",
+  "params": Object {
+    "depth": 1,
+  },
+}
+`);
+
+  expect(
+    createCategoriesEndpointRequest({
+      with: {
+        children: {
+          depth: 2,
+          includeHidden: true,
+        },
+      },
+    }),
+  ).toMatchInlineSnapshot(`
+Object {
+  "endpoint": "categories",
+  "method": "GET",
+  "params": Object {
+    "depth": 3,
+    "showHidden": "true",
+    "with": "descendants",
+  },
+}
+`);
+});
