4d9c11ad086f36fc081657b3d74c5d08ebbf3fc0
Timm Preetz <timm@preetz.us>
2021-03-28 10:40:58 +0200

    Implement resolve API

---
+++ src/endpoints/search/__tests__/resolve.test.ts
@@ -0,0 +1,49 @@
+import {
+  createSearchResolveEndpointRequest,
+  SearchResolveEndpointResponseData,
+} from '../resolve';
+
+it('Builds correct query', () => {
+  expect(createSearchResolveEndpointRequest({term: 'maxi rot'}))
+    .toMatchInlineSnapshot(`
+Object {
+  "endpoint": "search/resolve",
+  "method": "GET",
+  "params": Object {
+    "term": "maxi rot",
+  },
+}
+`);
+});
+
+it('Maps response correctly', () => {
+  const response: SearchResolveEndpointResponseData = {
+    matches: [
+      {
+        count: 1,
+        match: 'Maxi rot',
+        category: {
+          match: 'Maxi',
+          id: 500913,
+          name: 'Maxi',
+        },
+        attributes: [
+          {
+            match: 'rot',
+            name: 'color',
+            attributeGroup: 1,
+            attributeId: 38931,
+          },
+          {
+            match: 'kobaltblau',
+            name: 'colorDetail',
+            attributeGroup: 210,
+            attributeId: 38753,
+          },
+        ],
+      },
+    ],
+  };
+
+  expect(response).toBeTruthy();
+});
