50c217d2986c92093026cb392f0ec9ba30b2ad63
HenriBeck <henri.beck@scayle.com>
2024-03-07 11:00:42 +0100

    feat(storefront-api): Add filters to `CategorySearchSuggestion`

--- src/types/Search.ts
+++ src/types/Search.ts
@@ -1,12 +1,36 @@
 import {BapiCategory} from './BapiCategory';
 import {BapiProduct} from './BapiProduct';
 
-export type SearchEntity = CategorySearchSuggestion | ProductSearchSuggestion;
+export type SearchEntity =
+  | CategorySearchSuggestion
+  | ProductSearchSuggestion
+  | {type: undefined};
 
 export type CategorySearchSuggestion = {
   type: 'category';
   categorySuggestion: {
     category: BapiCategory;
+    filters: CategoryFilter[];
+  };
+};
+
+type CategoryFilter = CategoryAttributeFilter | {type: undefined};
+
+type CategoryAttributeFilter = {
+  type: 'attribute';
+  attributeFilter: {
+    group: {
+      id: number;
+      key: string;
+      label: string;
+      type: string;
+      multiSelect: boolean;
+    };
+    values: Array<{
+      id: number;
+      value: string;
+      label: string;
+    }>;
   };
 };
 
