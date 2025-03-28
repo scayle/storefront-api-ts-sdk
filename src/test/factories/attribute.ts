23ecd606c1d5735fc0f584278bd2b55b54163fb5
Luka Saric <luka.saric@scayle.com>
2025-03-28 12:36:35 +0000

    chore(sapi): add multi & single attr test factories

--- src/test/factories/attribute.ts
+++ src/test/factories/attribute.ts
@@ -1,10 +1,29 @@
-import type { AttributeGroup, AdvancedAttribute } from '../../types/Product'
+import type {
+  AttributeGroup,
+  AdvancedAttribute,
+  AttributeGroupMulti,
+  AttributeGroupSingle,
+} from '../../types/Product'
 import { Factory } from 'fishery'
 
+/**
+ * @deprecated - will be removed in the next major version in favor of using `AttributeGroupMulti` or `AttributeGroupSingle` directly.
+ */
 export const attributeGroupFactory: Factory<AttributeGroup> = Factory.define<
   AttributeGroup
->(() => {
-  return {
+>(() => ({
+  id: 1,
+  key: 'name',
+  label: 'Test Attribute',
+  type: null,
+  multiSelect: false,
+  values: {
+    label: 'Test Attribute',
+  },
+}))
+
+export const attributeGroupSingleFactory: Factory<AttributeGroupSingle> =
+  Factory.define<AttributeGroupSingle>(() => ({
     id: 1,
     key: 'name',
     label: 'Test Attribute',
@@ -13,21 +32,32 @@ export const attributeGroupFactory: Factory<AttributeGroup> = Factory.define<
     values: {
       label: 'Test Attribute',
     },
-  }
-})
+  }))
+
+export const attributeGroupMultiFactory: Factory<AttributeGroupMulti> = Factory
+  .define<AttributeGroupMulti>(() => ({
+    id: 1,
+    key: 'name',
+    label: 'Test Attribute',
+    type: null,
+    multiSelect: true,
+    values: [
+      {
+        label: 'Test Attribute',
+      },
+    ],
+  }))
 
 export const advancedAttributeFactory: Factory<AdvancedAttribute> = Factory
-  .define<AdvancedAttribute>(
-    () => ({
-      id: 553,
-      key: 'productName',
-      label: 'Produktname',
-      type: '',
-      values: [
-        {
-          fieldSet: [[{ value: "Jacke 'Premium Essentials'" }]],
-          groupSet: [],
-        },
-      ],
-    }),
-  )
+  .define<AdvancedAttribute>(() => ({
+    id: 553,
+    key: 'productName',
+    label: 'Produktname',
+    type: '',
+    values: [
+      {
+        fieldSet: [[{ value: "Jacke 'Premium Essentials'" }]],
+        groupSet: [],
+      },
+    ],
+  }))
